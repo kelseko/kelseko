f3bc64bb410c6eabfb2b312b8bad9d8f2cca056d
kelseko <90269003+kelseko@users.noreply.github.com>
2022-02-23 23:27:58 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,33 @@
+print("Hoşgeldiniz.... Hack araçları yükleniyor.......")
+apt update && apt upgrade
+pkg install python python2 python3 vim figlet curl
+pkg install php
+pip2 install lolcat
+pkg install git
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3
+print("1. Pack Yüklendi. Kullanmak için:
+cd TOOLSINSTALLERv3
+chmod +x TUANB4DUT.sh
+sh TUANB4DUT.sh")
+print("2.Pack Yükleniyor...")
+pkg update
+pkg install git
+git clone https://github.com/sundowndev/PhoneInfoga
+print("2.Pack Yüklendi. Kullanmak için:
+cd PhoneInfoga
+chmod 777 *
+python3 phoneinfoga.py -n +90*****
+Numarayı +90 şeklinde yazınız. Telefon numarasından bilgi toplamanızı sağlar.")
+print("3. Pack yükleniyor....")
+pkg update
+pkg uprage
+pkg upgrade
+pkg install python
+pkg install git
+git clone https://www.github.com/cyweb/hammer
+print("3. Pack Yüklendi.. Kullanmak için:
+cd hammer chmod +x hammer.py
+python hammer -s
+www.siteadresiyadaıpadresi.com -p 80 -t 135
+Hammer, siteleri çökertmek (DDOS ATTACK) sağlayan bir programdır sadece sitelere değil iplerede saldırı yapar.")
+print("Bizi tavsiye ettiğiniz için teşekkür ederim! Görüşmek üzere..")
